061ba79f4ac412bf790f7c0674d375645b31d289
kazuma takiuchi <basstrombone413@gmail.com>
2022-02-28 19:13:57 +0900

    fix base path

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "lt-template-2022-spring",
+  base: "/lt-template-2022-spring/",
   plugins: [react()],
 });
