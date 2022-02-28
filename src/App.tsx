255ec510329bde7fd4ee328b257b0d1fa092752a
kazuma takiuchi <basstrombone413@gmail.com>
2022-02-28 19:36:47 +0900

    add react-router

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,15 @@
+import { Route, Routes } from "react-router-dom";
+import About from "./pages/About";
+import Home from "./pages/Home";
+
 function App() {
   return (
     <div className="App">
-      <div>main</div>
+      <h1>Welcome to React Router!</h1>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="about" element={<About />} />
+      </Routes>
     </div>
   );
 }
