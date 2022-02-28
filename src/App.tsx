d9725187b5c0ebe9221083ff41f5657a1b369b1d
kazuma takiuchi <basstrombone413@gmail.com>
2022-02-28 21:38:26 +0900

    add chakraui

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,29 @@
+import { Button, IconButton } from "@chakra-ui/react";
 import { Route, Routes } from "react-router-dom";
 import About from "./pages/About";
 import Home from "./pages/Home";
+import { IoIosArrowBack, IoIosArrowForward } from "react-icons/io";
 
 function App() {
   return (
     <div className="App">
       <h1>Welcome to React Router!</h1>
+      <IconButton
+        aria-label="back"
+        icon={<IoIosArrowBack size="1.5em" />}
+        colorScheme="pink"
+        borderRadius={"50%"}
+        p="1"
+        m="1"
+      />
+      <IconButton
+        aria-label="forward"
+        icon={<IoIosArrowForward size="1.5em" />}
+        colorScheme="pink"
+        borderRadius={"50%"}
+        p="1"
+        m="1"
+      />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="about" element={<About />} />
--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,17 @@
+import { ChakraProvider } from "@chakra-ui/react";
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
-import App from "./App";
 import { BrowserRouter } from "react-router-dom";
+import App from "./App";
+import "./index.css";
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter basename="/lt-template-2022-spring">
-      <App />
-    </BrowserRouter>
+    <ChakraProvider>
+      <BrowserRouter basename="/lt-template-2022-spring">
+        <App />
+      </BrowserRouter>
+    </ChakraProvider>
   </React.StrictMode>,
   document.getElementById("root")
 );
